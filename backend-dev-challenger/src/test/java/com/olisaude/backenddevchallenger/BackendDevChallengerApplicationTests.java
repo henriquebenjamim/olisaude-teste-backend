076e6edf82033e39d3d59bd82e385e85fea7755f
henriquebenjamim <henriiqqq@gmail.com>
2023-09-02 20:11:48 -0300

    Feat: first commit, config

---
+++ backend-dev-challenger/src/test/java/com/olisaude/backenddevchallenger/BackendDevChallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olisaude.backenddevchallenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDevChallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
