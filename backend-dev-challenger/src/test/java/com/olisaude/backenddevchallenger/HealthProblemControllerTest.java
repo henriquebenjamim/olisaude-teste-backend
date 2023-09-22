1363031c01ebdba7238d7748effa003bd48118db
henriquebenjamim <henriiqqq@gmail.com>
2023-09-22 00:35:04 -0300

    Feat: addes 2 new test methods, GetById and his exception when fail

--- backend-dev-challenger/src/test/java/com/olisaude/backenddevchallenger/HealthProblemControllerTest.java
+++ backend-dev-challenger/src/test/java/com/olisaude/backenddevchallenger/HealthProblemControllerTest.java
@@ -10,13 +10,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
@@ -40,4 +47,41 @@ public class HealthProblemControllerTest {
         ResponseEntity<HealthProblem> response = assertDoesNotThrow(() -> healthProblemController.Post(healthProblemDTO));
         assertThat(response.getStatusCodeValue(), equalTo(HttpStatus.CREATED.value()));
     }
+
+    @Test
+    void shouldGetHealthProblemById() throws Exception {
+        Long healthProblemId = 1L;
+
+        HealthProblem mockHealthProblem = new HealthProblem();
+
+        mockHealthProblem.setId(healthProblemId);
+        mockHealthProblem.setDiseaseName("Diabetes");
+        mockHealthProblem.setDiseaseLevel(1);
+
+        when(healthProblemService.findHealthProblemById(healthProblemId)).thenReturn(mockHealthProblem);
+
+
+        HealthProblem returnedHealthProblem = assertDoesNotThrow(() -> healthProblemController.GetHealthProblemById(healthProblemId));
+
+        assertThat(mockHealthProblem.getId(), equalTo(returnedHealthProblem.getId()));
+        assertThat(mockHealthProblem.getDiseaseName(), equalTo(returnedHealthProblem.getDiseaseName()));
+        assertThat(mockHealthProblem.getDiseaseLevel(), equalTo(returnedHealthProblem.getDiseaseLevel()));
+    }
+
+    // ChatGpt Creates
+    @Test
+    void shouldHandleExceptionWhenIdNotFound() throws Exception {
+        Long healthProblemId = 1L; // Replace with a valid healthProblemId
+
+        // Mock the behavior of the service method to throw an exception
+        String exMessage = "Health problem not found";
+        when(healthProblemService.findHealthProblemById(healthProblemId)).thenThrow(new Exception(exMessage));
+
+        // Use assertThrows to verify that the exception is thrown
+        Exception exception = assertThrows(Exception.class, () -> healthProblemController.GetHealthProblemById(healthProblemId));
+
+        // Perform assertions on the exception message or type as needed
+        assertThat(exMessage, equalTo(exception.getMessage()));
+    }
+
 }
